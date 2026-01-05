0a7eeeb460edc8b67a890dc32c0a45d2e8016dd8
CholponDev <januzakova.cholpon@gmail.com>
2026-01-05 10:20:25 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+let html_img = document.getElementById("photo")
+
+// console.log(html_img.hasAttribute('alt'))
+
+// console.log(html_img.getAttribute('id'))
+
+// html_img.setAttribute('src','https://media.licdn.com/dms/image/v2/D5612AQE8NiooxTxA3w/article-cover_image-shrink_720_1280/article-cover_image-shrink_720_1280/0/1695825196046?e=2147483647&v=beta&t=2zU71mKLxGihkNQB5eMDjCgbD7srasN1gyEqowXMGV4')
+
+
+
+const Addphoto = () => {
+    html_img.setAttribute('src','https://media.licdn.com/dms/image/v2/D5612AQE8NiooxTxA3w/article-cover_image-shrink_720_1280/article-cover_image-shrink_720_1280/0/1695825196046?e=2147483647&v=beta&t=2zU71mKLxGihkNQB5eMDjCgbD7srasN1gyEqowXMGV4')
+}
